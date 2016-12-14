8137280871a64c73024ae870f780c6665589de6a
GBarzaq <0ago0barzaq@gmail.com>
2016-12-14 22:49:13 +0200

    Add files via upload

---
+++ university/create.sql
@@ -0,0 +1,54 @@
+--campus
+
+--professor: object, varray 5
+
+--units: varray 5 varchar2
+
+--school
+
+--department
+
+--research-center
+
+--faculty
+
+--equipments: varray 3 varchar2 
+
+--building
+
+--office 
+
+--classroom
+
+--lab
+
+--degree
+
+--person
+
+--staff
+
+--student
+
+--admin
+
+--technician
+
+--lecturer
+
+--senior_lecturer
+
+--associate_lecturer
+
+--tutor
+
+--enrolls_in
+
+--BODY OF OFFICE -SONDOS
+
+--subject
+
+--takes
+
+
+
