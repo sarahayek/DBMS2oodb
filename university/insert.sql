a359a052cfc535ac4ab2f05669cbc0504b6263b4
SayaKh <aasmaa5566@icloud.com>
2016-12-14 23:40:45 +0200

    Update insert.sql

--- university/insert.sql
+++ university/insert.sql
@@ -17,6 +17,10 @@
 --lab
 
 --degree
+BEGIN
+  Degree_T.insert_degree('D100','Bachelor of Comp.Sci','3','Year 12 or equivalent','4');
+  Degree_T.insert_degree('D101','Master of Comp.Sci','2','Bach of Comp.Sci','4');
+END;
 
 --person
 
