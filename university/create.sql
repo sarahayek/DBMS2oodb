13744bf08e5d31d27ac5dd74929c7594179d0dc7
sondos <sondos21.11@gmail.com>
2016-12-15 21:45:49 +0200

    Update create.sql

--- university/create.sql
+++ university/create.sql
@@ -136,7 +136,7 @@ END;
 create or replace type body office_T as
 member procedure show_office is
 cursor c_office is
-select s.pers_fname, b.off_no, b.off_phone
+select s.pers_surname, b.off_no, b.off_phone
 from person p, build b, office o, staff s
 where b.bld_id = self.bld_id and b.bld_id =
 o.bld_id
@@ -144,12 +144,12 @@ and p.pers_id = s.pers_id and s.in_office = ref
 (o);
 begin
 dbms_output.put_line
-(‘name’||’ ‘||’office no’||’ ‘||‘office
+(‘surname’||’  ‘||’office no’||’  ‘||‘office
 phone’);
 
 for v_office in c_office loop
 dbms_output.put_line
-(v_office.pers_name||’ ‘||
+(v_office.pers_surname||’ ‘||
 v_office.off_no||’ ‘||
 v_office.off_phone);
 end loop;
